3f806a42f4457fba718c63b2c118c51a496886e5
derimer <jderimer@gmail.com>
2025-04-04 20:21:41 +0200

    Fix: remove cached gh-pages repo from index

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,30 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: './index.js', // Point d'entrée
+  entry: './index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'), // Dossier de sortie
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   },
-  mode: 'production'
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/images/[name][ext]'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/assets/images', to: 'assets/images' },
+        { from: 'index.html', to: '' }
+      ]
+    })
+  ]
 };
